233168e697cf0ca046a4ef76320a46d6fcf01228
claudiu <claudiu.garba@gmail.com>
2014-02-01 17:45:25 +0100

    bleah

--- obj/main.c
+++ obj/main.c
@@ -10,9 +10,23 @@
 
 int main(int argc, const char * argv[])
 {
+//    short i;
+//    int l;
+//    long ll;
+//    float huge;
+//    double not;
+//    char s;
+//    struct GeoLocation{
+//        float lat;
+//        float lgn;
+//    };
 
     // insert code here...
     printf("Hello, World!\n");
+    float w = 15.2;
+    printf("this is %f:\n", w);
+    float timeOfCooking =  w * 12.3;
+    printf("time for cooking: %f\t\t minutes\n", timeOfCooking);
     return 0;
 }
 
