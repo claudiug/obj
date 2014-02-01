27cfc805a4508fd2a8798e7af43320e6581e96eb
claudiu <claudiu.garba@gmail.com>
2014-02-01 17:36:15 +0100

    Initial Commit

---
+++ obj/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  obj
+//
+//  Created by claudiu on 2/1/14.
+//  Copyright (c) 2014 claudiu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
